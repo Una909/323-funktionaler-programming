e08199a99f4dcb08c11384077e140ab9167cb9c0
Anukul <sommer.anukul99@gmail.com>
2024-02-17 15:25:16 +0100

    Aufgabe 1

--- immutable_objects.js
+++ immutable_objects.js
@@ -1,3 +1,30 @@
 // Primitive data that cant be changed
 const number = 42;
 
+// Const variable with 3 properties
+const book = {
+    name: "fantasy book",
+    author: "bookman",
+    pages: 687,
+};
+
+// adding a new property
+const newInfo = {
+    ...book,
+    language: "eng"
+};
+
+console.log(newInfo);
+
+// Update already existing properties
+const updateInfo = {
+    ...newInfo,
+    name: "Fire and Ice"
+};
+
+console.log(updateInfo);
+
+// Deleting a property
+const {pages, ...deleteInfo} = updateInfo;
+
+console.log(deleteInfo);
